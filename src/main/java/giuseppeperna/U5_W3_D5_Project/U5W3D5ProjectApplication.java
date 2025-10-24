9a88046d659cdd78042a387e1cbe4a17dc0acbac
PernaGiuseppe <giuseppeperna97@hotmail.com>
2025-10-24 10:36:16 +0200

    Initial commit

---
+++ src/main/java/giuseppeperna/U5_W3_D5_Project/U5W3D5ProjectApplication.java
@@ -0,0 +1,13 @@
+package giuseppeperna.U5_W3_D5_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W3D5ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W3D5ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/giuseppeperna/U5_W3_D5_Project/U5W3D5ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package giuseppeperna.U5_W3_D5_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W3D5ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
